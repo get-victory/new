9f3295ceefb1cf5f99053c182ae53592372b9781
shuang <793838827@qq.com>
2020-08-12 15:19:37 +0800

    登录页完成，配置导航守卫

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="login">
-    <el-form label-width="80px" :model="form" ref="form" :rules="rules">
+    <el-form status-icon label-width="80px" :model="form" ref="form" :rules="rules">
       <img src="../assets/avatar.jpg" alt="" class="logo">
       <el-form-item label="用户名" prop="username">
         <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input placeholder="请输入密码" v-model="form.password"></el-input>
+        <el-input placeholder="请输入密码" v-model="form.password" type="password"></el-input>
       </el-form-item>
      <el-form-item>
         <el-button @click="Loginbtn"  class="btn" type="primary">登录</el-button>
@@ -37,13 +37,35 @@ export default {
     }
   },
   methods: {
-    Loginbtn () {
-
-    },
     ResetForm () {
       // 获取到form组件，调用组件的重置方法
       this.$refs.form.resetFields()
+    },
+    async Loginbtn () {
+      // 先进行校验，再发送请求
+      try {
+        // 先校验，再发送请求
+        await this.$refs.form.validate()
+        // 解构
+        const { meta, data } = await this.$axios.post('login', this.form)
+        if (meta.status === 200) {
+          // 登录成功，先存储token令牌
+          localStorage.getItem('token', data.token)
+          this.$message({
+            message: meta.msg,
+            type: 'success',
+            duration: 1000
+          })
+          // 登录成功，要跳转到首页
+          this.$router.push({ name: 'index' })
+        } else {
+          this.$message.error(meta.msg)
+        }
+      } catch (e) {
+        console.log(e)
+      }
     }
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,37 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
+// axios 配置,将axios挂在到vue实例上，所有原型实例可以共享
+Vue.prototype.$axios = axios
+// 配置基准地址
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+// 配置axios请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 拦截每次请求，携带token
+  config.headers.Authorization = localStorage.getItem('token')
+  return config
+},
+// 对错误做些处理
+function (error) {
+  return Promise.reject(error)
+})
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+  response = response.data
+  // 对响应统一拦截，若返回401，说明token无效或过期，返回到登录页
+  if (response.meta.status === 401) {
+    response.meta.msg = '你好，你的令牌已过期，请重新登录'
+    // 去除过期的token
+    localStorage.removeItem('token')
+    router.push('/login')
+  }
+  return response
+}, function (error) {
+  // 对错误信息进行处理
+  return Promise.reject(error)
+})
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
